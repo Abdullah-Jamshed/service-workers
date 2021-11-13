ea0deb2b17c5a837d1ad766ac57b830524cd69d1
abdullah jamshed <abdullahjamshad01@gmail.com>
2021-11-13 22:31:25 +0500

    dynamic cahce

--- sw2.js
+++ sw2.js
@@ -70,3 +70,26 @@ self.addEventListener('fetch', evt => {
     })
   );
 });
+
+
+
+
+
+
+// dynamic chache
+
+// self.addEventListener('fetch', function (event) {
+//   event.respondWith(
+//     caches.open('mysite-dynamic').then(function (cache) {
+//       return cache.match(event.request).then(function (response) {
+//         return (
+//           response ||
+//           fetch(event.request).then(function (response) {
+//             cache.put(event.request, response.clone());
+//             return response;
+//           })
+//         );
+//       });
+//     }),
+//   );
+// });
