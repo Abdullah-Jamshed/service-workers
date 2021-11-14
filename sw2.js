0671c20bf6b2af66f1246fe5ebe38cdfe3bcf280
abdullah jamshed <abdullahjamshad01@gmail.com>
2021-11-14 22:34:23 +0500

    extra code remove

--- sw2.js
+++ sw2.js
@@ -70,26 +70,3 @@ self.addEventListener('fetch', evt => {
     })
   );
 });
-
-
-
-
-
-
-// dynamic chache
-
-// self.addEventListener('fetch', function (event) {
-//   event.respondWith(
-//     caches.open('mysite-dynamic').then(function (cache) {
-//       return cache.match(event.request).then(function (response) {
-//         return (
-//           response ||
-//           fetch(event.request).then(function (response) {
-//             cache.put(event.request, response.clone());
-//             return response;
-//           })
-//         );
-//       });
-//     }),
-//   );
-// });
